26bcffcb23cf33a85eb8dc3dd97db0807109f806
Elena Borisova <elenaborrisova@gmail.com>
2023-02-28 17:34:17 +0100

    add constant Board class variables and reformat code

--- game/board.py
+++ game/board.py
@@ -3,6 +3,8 @@ from player import Player
 
 class Board:
     SIZE = 5
+    FREE_ZONE_SYMBOL = '@'
+    FREE_ZONE_POSITIONS = ((1, 2), (2, 1), (2, 3), (3, 2))
 
     def __init__(self):
         self.board = [['.' for _ in range(Board.SIZE)] for _ in range(Board.SIZE)]
@@ -12,17 +14,12 @@ class Board:
             self.board[zone[0]][zone[1]] = player.name[0]
 
     def mark_free_zones(self):
-        POSITIONS = ((1, 2), (2, 1), (2, 3), (3, 2))
-        SYMBOL = '@'
-
-        for pos in POSITIONS:
-            self.board[pos[0]][pos[1]] = SYMBOL
+        for pos in Board.FREE_ZONE_POSITIONS:
+            self.board[pos[0]][pos[1]] = Board.FREE_ZONE_SYMBOL
 
     def __repr__(self):
-        formatted_board = ''
         delimiter = '   '
-
-        formatted_board += '    ' + delimiter.join(list(map(str, range(Board.SIZE)))) + '\n'
+        formatted_board = '    ' + delimiter.join(list(map(str, range(Board.SIZE)))) + '\n'
         formatted_board += '    ' + '___' * (Board.SIZE + 1) + '\n'
 
         for r in range(Board.SIZE):
